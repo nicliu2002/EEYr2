8eac522593c83e646403d6f1b7fbf74dc620ddb5
nicliu2002@gmail.com <922821>
2022-04-03 07:30:41 +1000

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,10 @@ public class Main {
         Word testWord = new Word("cat");
         System.out.println("getDigit\t" + testWord.getDigit('B'));
         System.out.println("getNumWords\t" + pw.getNumWords());
-        System.out.println("isKnown (false), second one will be true \t" + pw.isKnown(testWord) + "\n");
+        System.out.println("isKnown (false), second one will be true \t" + pw.isKnown(testWord));
         pw.addWord("cat");
-        System.out.println(pw.isKnown(testWord));
-        System.out.println("listWords, s \n"+ pw.listWords("823"));
+        System.out.print(pw.isKnown(testWord));
+        System.out.println("listWords \n"+ pw.listWords("823"));
         System.out.println("listWords for new code (using hashmap) \t" + npw.listWords("823"));
 
     }
